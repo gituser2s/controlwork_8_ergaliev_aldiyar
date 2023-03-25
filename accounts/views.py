d7d25e3c2939c8d20240ebb73674aeaec447f209
Nickname Lastname <adada2231wadsa324@gmail.com>
2023-03-25 19:06:18 +0600

    Файл представлений Логина и Регистрации пользователя, а также смены юзера и его отображении

---
+++ accounts/views.py
@@ -0,0 +1,92 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
+from accounts.forms import LoginForm, CustomUserCreationForm, UserChangeForm
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            messages.error(request, "Некорректные данные!")
+            return redirect('index')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            messages.warning(request, "Пользователь в бд не найден!")
+            return redirect('index')
+        login(request, user)
+        messages.success(request, 'Добро пожаловать!')
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = "/"
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.success_url)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 3
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        review = self.object.review_products.order_by('text')
+        paginator = Paginator(review, self.paginate_related_by,
+                              orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['reviews'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
+
+
+class UserChangeView(UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'pk': self.object.pk})
